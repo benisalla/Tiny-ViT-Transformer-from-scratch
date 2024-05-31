f1aa2dda2405889bbb13ff74efc2c2b17308eb02
ben alla ismail <ismailbenalla52@gmail.com>
2024-05-31 22:00:58 +0300

    solve some issues and add some style

--- tiny_vit_transformer_from_scratch/app/main.py
+++ tiny_vit_transformer_from_scratch/app/main.py
@@ -1,3 +1,4 @@
+import base64
 import streamlit as st
 from tiny_vit_transformer_from_scratch.model.vit_transformer import VisionTransformer
 import torch
@@ -5,14 +6,11 @@ from torchvision import transforms
 from PIL import Image
 
 def load_from_checkpoints(checkpoint_path, device):
-    """
-    Loads the Tiny-ViT model checkpoint, maps it to the appropriate device, and initializes the optimizer if present.
-    """
     checkpoint = torch.load(checkpoint_path, map_location=device)
     model = VisionTransformer(**checkpoint['model_args'])
-    model.to(device)  
-    model.eval()  
-    
+    model.to(device)
+    model.eval()
+
     try:
         model.load_state_dict(checkpoint['model'], strict=True)
     except RuntimeError as e:
@@ -25,48 +23,45 @@ def load_from_checkpoints(checkpoint_path, device):
 
     return model
 
-def add_background_image():
+def add_background_image(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode()
+    with open("tiny_vit_transformer_from_scratch/app/style/style.css", "r") as css_file:
+        css_content = css_file.read()
+        css_content = css_content.replace("{encoded_image}", encoded_image)
     st.markdown(
-        """
-        <style>
-        .stApp {
-            background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-            background-size: cover;
-        }
-        </style>
-        """,
+        f"<style>{css_content}</style>",
         unsafe_allow_html=True
     )
 
 def main():
-    add_background_image()
+    add_background_image("tiny_vit_transformer_from_scratch/app/src/image.png")
     
-    st.title("🖼️ Tiny-ViT Image Classification App")
-    st.write("Upload an image, and the Tiny-ViT model will classify it.")
-    st.sidebar.title("Settings")
+    st.markdown('<div class="title-container"><h1 class="title">🖼️ Tiny-ViT Image Classification App</h1></div>', unsafe_allow_html=True)
+    st.markdown('<div class="upload-container"><p class="instructions">Upload an image, and the Tiny-ViT model will classify it.</p><p class="classes">Current classes the model can detect are: Tomato Bacterial spot, Tomato Early blight, Tomato Late blight, Tomato Leaf Mold</p></div>', unsafe_allow_html=True)
     
-    # Path and device setup
+    st.sidebar.markdown('<div class="sidebar-title">Tiny-Vit</div>', unsafe_allow_html=True)
+    
     checkpoint_path = "C:\\Users\\Omar\\Desktop\\Week-end-projects\\Tiny-ViT-Transformer-from-scratch\\tiny_vit_transformer_from_scratch\\checkpoints\\vit_chpts.pth"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Load model
     model = load_from_checkpoints(checkpoint_path, device)
-    
-    # Define classes
+
     classes = [
         "Tomato Bacterial spot", "Tomato Early blight", "Tomato Late blight", "Tomato Leaf Mold",
         "Tomato Septoria leaf spot", "Tomato Spider mites Two spotted spider mite", "Tomato Target Spot",
         "Tomato healthy", "Potato Early blight", "Potato Late blight", "Tomato Tomato mosaic virus", "Potato healthy"
     ]
-    
+
     st.sidebar.header("Upload Image")
-    uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.sidebar.file_uploader("", type=["jpg", "jpeg", "png"])
     
     if uploaded_file is not None:
         image = Image.open(uploaded_file).convert("RGB")
+        st.markdown('<div class="image-container">', unsafe_allow_html=True)
         st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.markdown('</div>', unsafe_allow_html=True)
 
-        # Image preprocessing
         im_size = 256 
         image_transforms = transforms.Compose([
             transforms.Resize((im_size, im_size)),
@@ -75,15 +70,14 @@ def main():
         ])
         input_tensor = image_transforms(image).unsqueeze(0).to(device)
 
-        # Prediction
         with torch.no_grad():
             output = model(input_tensor)
             _, predicted_class_idx = torch.max(output, 1)
             predicted_class = classes[predicted_class_idx.item()]
 
-        st.success(f"**Predicted Class:** {predicted_class}")
+            st.markdown(f'<div class="prediction-result">Predicted Class: {predicted_class}</div>', unsafe_allow_html=True)
     else:
-        st.info("Please upload an image to classify.")
+        st.markdown('<div class="upload-container"><p class="instructions">Please upload an image to classify.</p></div>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
